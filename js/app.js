1c06c0208c1801f617ba0f44c5ac4f517d6e69de
JeisonLeonel <jeisonleonel0814@gmail.com>
2022-12-20 14:40:30 -0500

    Se realiza la integracion del API

---
+++ js/app.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    const random = getRandomInt(1,201);
+    feachData(random);
+})
+
+const getRandomInt=(min,max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+const feachData = async(id) => {
+    try{
+        const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
+        const data = await res.json();
+        pintCard(data);
+        console.log(data);
+    }
+    catch(error){
+    }
+}
+
+const pintCard  = (personaje) => {
+        
+            const main = document.querySelector('.content-main');
+            const template = document.getElementById('grid-template').content;
+            const clone = template.cloneNode(true);
+            const fragment = document.createDocumentFragment();
+            clone.querySelector('.img-api').setAttribute('src', personaje.image);
+            clone.querySelector('#nombre').innerHTML = personaje.name;
+            clone.querySelector('#especie').innerHTML = `${personaje.status} - <span>${personaje.species}</span>`;
+            clone.querySelector('#lugar').innerHTML = personaje.location.name;
+            clone.querySelector('#episodio').innerHTML = personaje.origin.name;
+            fragment.appendChild(clone);
+            main.appendChild(fragment); 
+}
