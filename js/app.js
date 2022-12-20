663fc81688351ee6c32b52687cedf293b72101f9
JeisonLeonel <jeisonleonel0814@gmail.com>
2022-12-20 18:19:38 -0500

    Se completo el proyecto de la API

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () =>{
-    const random = getRandomInt(1,201);
+    const random = getRandomInt(1,42);
     feachData(random);
 })
 
@@ -7,28 +7,34 @@ const getRandomInt=(min,max) => {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-const feachData = async(id) => {
+const feachData = async(pages) => {
     try{
-        const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
+        const res = await fetch(`https://rickandmortyapi.com/api/character/?page=${pages}`)
         const data = await res.json();
+
         pintCard(data);
-        console.log(data);
     }
     catch(error){
     }
 }
 
-const pintCard  = (personaje) => {
-        
-            const main = document.querySelector('.content-main');
-            const template = document.getElementById('grid-template').content;
+    const main = document.getElementById('content-main');
+    const template = document.getElementById('grid-template').content;
+    const fragment = document.createDocumentFragment();
+    
+const pintCard  = data => {
+    Object.values(data.results).forEach((personaje) => {
+        console.log(personaje);
+
+            template.querySelector('.img-api').setAttribute('src', personaje.image);
+            template.querySelector('#nombre').innerHTML = personaje.name;
+            template.querySelector('#especie').innerHTML = `${personaje.status} - <span>${personaje.species}</span>`;
+            template.querySelector('#lugar').innerHTML = personaje.location.name;
+            template.querySelector('#episodio').innerHTML = personaje.origin.name;
+
             const clone = template.cloneNode(true);
-            const fragment = document.createDocumentFragment();
-            clone.querySelector('.img-api').setAttribute('src', personaje.image);
-            clone.querySelector('#nombre').innerHTML = personaje.name;
-            clone.querySelector('#especie').innerHTML = `${personaje.status} - <span>${personaje.species}</span>`;
-            clone.querySelector('#lugar').innerHTML = personaje.location.name;
-            clone.querySelector('#episodio').innerHTML = personaje.origin.name;
             fragment.appendChild(clone);
+    })            
             main.appendChild(fragment); 
+            console.log(main)
 }
